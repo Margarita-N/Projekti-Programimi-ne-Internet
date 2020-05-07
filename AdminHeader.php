bb7e3eb80c45d2a2245606ccc70a6d1154083a8f
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-05-07 15:17:03 +0200

    Header i admin homepage qe te behet resuse

---
+++ AdminHeader.php
@@ -0,0 +1,36 @@
+<header>
+        <div class="nav-menu">
+            <div class="home-menu">
+            <ul>
+                <li><a href="AdminMain.html"><i class="fas fa-home" style="color:white;font-size: 30px;"></i></a></li>
+                <li><a href="#">Log out</a></li>
+            </ul>
+            </div>
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown"><a href="#">Users</a>
+                    <div class="dropdown-menu">
+                        <ul>
+                            <li><a href="#" class="dropdown-item">Manage Users</a></li>
+                            <li><a href="#" class="dropdown-item">Add user</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item dropdown"><a href="#">Books</a>
+                    <div class="dropdown-menu">
+                        <ul>
+                            <li><a href="#" class="dropdown-item">Manage Books</a></li>
+                            <li><a href="#" class="dropdown-item">Add book</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item dropdown"><a href="#">Categories</a>
+                    <div class="dropdown-menu">
+                        <ul>
+                            <li><a href="#" class="dropdown-item">Manage Categories</a></li>
+                            <li><a href="#" class="dropdown-item">Add Categories</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </header>
