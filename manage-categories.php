1b6b75e913c736458530de9db1d39b87d826f351
MihrijeK <42340737+MihrijeK@users.noreply.github.com>
2020-05-08 21:53:30 +0200

    Menaxhimi i kategorive

---
+++ manage-categories.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+    {   
+header('location:index.php');
+}
+else{ 
+if(isset($_GET['del']))
+{
+$id=$_GET['del'];
+$sql = "delete from kategorite  WHERE kategoriID=:id";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':id',$id, PDO::PARAM_STR);
+$query -> execute();
+$_SESSION['delmsg']="Category deleted scuccessfully ";
+header('location:manage-categories.php');
+
+}
+    ?>
+<!DOCTYPE html>
+<html>
+ <head>
+        <title>Manage</title>
+        <link href="css/AdminStyle.css" rel="stylesheet"/>
+        <link href="css/style.css" rel="stylesheet"/>
+        <link href="css/all.min.css" rel="stylesheet"/>
+        <script src="css/all.min.js"></script>
+    </head>
+<body>
+    <style>
+        <?php
+        readfile("css/AdminStyle.css");
+        ?>
+    </style>
+    <div class="center-body">
+        <?php require("AdminHeader.php");
+              require("db/dbconfig.php");
+              //Add a search bar to search by name and refresh by ajax
+              ?>
+                                <table class="content-table" style="">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Kategorite</th>
+                                            <th>Statusi</th>
+                                            <th>Edit</th>
+											<th>Delete</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+<?php $sql = "SELECT * from  kategorite";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{               ?>                                      
+                                        <tr >
+                                            <td class="center"><?php echo htmlentities($cnt);?></td>
+                                            <td class="center"><?php echo htmlentities($result->kategoriName);?></td>
+                                            <td class="center"><?php if($result->Statusi==1) {?>
+                                            <a href="#" >Aktive</a>
+                                            <?php } else {?>
+                                            <a href="#" >Joaktive</a>
+                                            <?php } ?></td>
+                                            <td class="center"><a href="edit-category.php?catid=<?php echo htmlentities($result->kategoriID);?>"> Edit</a></td> 
+                                          <td class="center"><a href="manage-categories.php?del=<?php echo htmlentities($result->kategoriID);?>" onclick="return confirm('Are you sure you want to delete?');"" >Delete</a></td>
+                                            </td>
+                                        </tr>
+										
+                         <?php $cnt=$cnt+1;}} ?>                                      
+                                    </tbody>
+                                </table>
+                            </div>
+                        
+</body>
+</html>
+<?php } ?>
