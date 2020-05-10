3df3b80270693d803ba507b7764a81b02174e401
LyraPireva <58956000+LyraPireva@users.noreply.github.com>
2020-05-10 22:08:56 +0200

    Fillimi i footer te User(Kontakti)

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="footer_part">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-sm-6 col-lg-2">
+                    <div class="single_footer_part">
+                        <h4 style="color: lightsalmon;">KONTAKTI</h4>
+                       <p style="color:white;"> Per kontakt direkt me ne<br/>
+
+                        Tel: +38344111111</br>
+                        Fax: (07) 30367095
+                    </p>
+                    </div>
+                </div>
