159da9df7c3af07e1dc507ea54d0af947950d183
LyraPireva <58956000+LyraPireva@users.noreply.github.com>
2020-05-10 22:10:33 +0200

    Info shtese ne footer te user

--- footer.php
+++ footer.php
@@ -11,3 +11,11 @@
                     </p>
                     </div>
                 </div>
+        
+        
+            <div class="col-sm-6 col-lg-3">
+                    <div class="single_footer_part">
+                        <h4 style="color: lightsalmon;">NGA NE PER JU</h4>
+                       <p style="color: white;">Kjo faqe ofron shume mundesi per adhuruesit e librit,ku pervec blerjes online dhe te lehte ju mund te ndani eksperiencen tuaj edhe me te tjeret.Kenaqesia juaj ne lexim ,kenaqesia jone ne pune !</p>
+                    </div>
+                </div>
