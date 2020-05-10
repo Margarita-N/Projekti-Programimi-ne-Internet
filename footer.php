b304b883b09f45426d7713cf9c584022d9c9bc31
LyraPireva <58956000+LyraPireva@users.noreply.github.com>
2020-05-10 23:21:07 +0200

    Pjesa e footerit me linqe per qasje ne to (UserFooter)

--- footer.php
+++ footer.php
@@ -12,6 +12,18 @@
                     </div>
                 </div>
         
+                 <div class="col-sm-6 col-lg-2">
+                    <div class="single_footer_part">
+                        <h4 style="color:lightsalmon;">QASJE E SHPEJTE</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="https://www.biblioteka-ks.org/">BIBLIOTEKA KOMBETARE</a></li>
+                            <li><a href="https://www.uni-pr.edu/">UNIVERSITETI I PRISHTINES</a></li>
+                            <li><a href="http://www.biblioteka-pr.org/sq/Home">BIBLIOTEKA "HIVZI SULEJMANI"</a></li>
+                            <li><a href="http://dukagjinigroup.com/project/libraria-dukagjini/">LIBRARIA DUKAGJINI</a></li>
+                        </ul>
+                    </div>
+                </div>
+
         
             <div class="col-sm-6 col-lg-3">
                     <div class="single_footer_part">
@@ -19,3 +31,4 @@
                        <p style="color: white;">Kjo faqe ofron shume mundesi per adhuruesit e librit,ku pervec blerjes online dhe te lehte ju mund te ndani eksperiencen tuaj edhe me te tjeret.Kenaqesia juaj ne lexim ,kenaqesia jone ne pune !</p>
                     </div>
                 </div>
+        
