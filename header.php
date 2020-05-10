d00d3ef46312d2029f702d62438fa108be56dc63
LyraPireva <58956000+LyraPireva@users.noreply.github.com>
2020-05-10 06:20:47 +0200

    Krijimi i navigation bar per User

---
+++ header.php
@@ -0,0 +1,18 @@
+ <header class="main_menu home_menu">
+        <div class="container-fluid">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-lg-11">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <h1 class="contominant" style="font-family: aclonica;margin-top:8px;">BiblioTech</h1>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="menu_icon"><i class="fas fa-bars"></i></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
+                           
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.html">BALLINA</a>
+                                </li>
