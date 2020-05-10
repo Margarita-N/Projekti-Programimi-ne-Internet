a4eaad95337e4439db97fe646908827652638604
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-05-10 22:30:30 +0200

    Krijimi i lidhjes dhe rregullimi me prepare statement

--- validate.php
+++ validate.php
@@ -7,4 +7,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     if($email=="" || $password==""){
         echo '<span class="red">All fields must be filled!</span>';
     }
+    else{
+    require("db/dbconfig.php");
+    $conn=new mysqli(serverName,username,password,dbname);
+    
+
+    $stmt=$conn->prepare("SELECT * FROM perdoruesit WHERE email=? and passwordi=?");
+    $stmt->bind_param("ss",$email,$password);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $resultset=$result->fetch_all(MYSQLI_ASSOC);
     ?>
