04884915bf6b46c4301b6e6dec7df83eda85f03c
LyraPireva <58956000+LyraPireva@users.noreply.github.com>
2020-05-11 03:52:00 +0200

    Ballina-pjesa e banner

--- index.php
+++ index.php
@@ -32,3 +32,22 @@
     <?php
     require("header.php");
     ?>
+<section class="banner_part">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-7">
+                    <div class="banner_slider">
+                        <div class="single_banner_slider">
+                            <div class="banner_text">
+                                <div class="banner_text_iner">
+                                    <h5>Platforme online per shitjen e librave</h5>
+                                    <h1>So many books,so little time...</h1>
+                                   
+                           </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>  
