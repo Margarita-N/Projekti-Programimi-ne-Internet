bd03e3ddb4c00523bcca2b95e4222116e60919d5
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-05-12 00:10:10 +0200

    Paraqitja e errorit nese fushat jane te zbrazta, nese ekziston nje account tashme dhe nese email nuk eshte valide

--- validate-signup.php
+++ validate-signup.php
@@ -11,5 +11,21 @@
         $state=$_POST['state'];
 
         $passwordLength=strlen($password);
+        if($username=="" || $email=="" || $password=="" || $confirmPassword==""){
+            echo '<span class="red">All fields must be filled!</span>'.$passwordLength;
+        }
+        else{
+            $stmt=$conn->prepare("SELECT * FROM perdoruesit WHERE email=?");
+            $stmt->bind_param("s",$email);
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+            $resultset=$result->fetch_all(MYSQLI_ASSOC);
+            if(sizeof($resultset)>0){
+                echo '<span class="red">Email i dhene eshte i regjistruar ne nje account!</span>';
+            }
+            elseif(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+                echo '<span class="red">Email is not valid!</span>';
+            }
   }
   ?>
