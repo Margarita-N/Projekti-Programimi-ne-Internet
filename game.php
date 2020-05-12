6dc2c35dc53ac4db7032992bbf2b4403ece64bc0
LyraPireva <58956000+LyraPireva@users.noreply.github.com>
2020-05-12 15:45:16 +0200

    Funksioni wordsarray-per leximin e file

--- game.php
+++ game.php
@@ -43,5 +43,34 @@
     $MAX_ATTEMPTS=5;            
     $WORDLISTFILE='wordlist.txt';
 
+   function wordsarray($wordFile)
+{
+    $file = fopen($wordFile,'r');
+       if ($file)
+    {
+        $random_line = null;
+        $line = null;
+        $count = 0;
+        while (($line = fgets($file)) != false) 
+        {
+            $count++;
+            if(rand() % $count == 0) 
+            {
+                  $random_line = trim($line);
+            }
+    }
+    if (!feof($file)) 
+    {
+        fclose($file);
+        return null;
+    }else 
+    {
+        fclose($file);
+    }
+}
+    $answer = str_split($random_line);
+    return $answer;
+}
+
 
 
