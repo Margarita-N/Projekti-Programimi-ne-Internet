77934c56a5c76eb901562f7b00b830bb8e7f3aaf
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-05-12 16:51:14 +0200

    Krijimi i sessionit kur konfirmohet email dhe password dhe vendosja e cookie nese selektohet remember me

--- validate.php
+++ validate.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+require("user-class.php");
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $email=$_POST['email'];
     $password=$_POST['password'];
@@ -11,18 +13,37 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     require("db/dbconfig.php");
     $conn=new mysqli(serverName,username,password,dbname);
     
-
+    $hashedPass=md5($password);
     $stmt=$conn->prepare("SELECT * FROM perdoruesit WHERE email=? and passwordi=?");
-    $stmt->bind_param("ss",$email,$password);
+    $stmt->bind_param("ss",$email,$hashedPass);
     $stmt->execute();
 
     $result = $stmt->get_result();
     $resultset=$result->fetch_all(MYSQLI_ASSOC);
-         
-        if(sizeof($resultset)>0){
+
+    if(sizeof($resultset)>0){
+        
+        $query='SELECT * FROM perdoruesit WHERE email="'.$email.'"';
+        $rowSet=mysqli_query($conn,$query);
+        while($row=$rowSet->fetch_assoc()){
+            $id=$row['userID'];
+            $username=$row['username'];
+        }
+
+        if(isset($_SESSION['user'])){
+            unset($_SESSION['user']);
+        }
+
+        $loggedInUser=new user($id,$username,$password,$email);
+        $_SESSION['user']=$loggedInUser;
+
         echo 'You are logged in!';
         if($remember==1){
-            //vendos cookie per te mbajtur mend perdoruesin
+            if(isset($_COOKIE['rememberedUser'])){
+                setcookie('rememberedUser',time()-(86400*30));
+            }
+
+            setcookie('rememberedUser',serialize($loggedInUser),time()+(86400*30));
         }
     }
     else{
@@ -30,4 +51,4 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     }
 }
 }
-    ?>
+?>
