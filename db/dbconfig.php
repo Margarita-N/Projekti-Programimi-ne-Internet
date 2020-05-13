5eca4b1435c6fcb9cc184f27c26a73e3624f8eff
MihrijeK <42340737+MihrijeK@users.noreply.github.com>
2020-05-13 23:55:49 +0200

    Funksioni getData i perdorur per marrjen e librave ne faqen  index

--- db/dbconfig.php
+++ db/dbconfig.php
@@ -3,4 +3,14 @@ define("serverName","localhost");
 define("dbname","bibliotech");
 define("username","root");
 define("password","");
+
+function getData(){
+        $sql = "SELECT bookID,bookName,price,fotoPath FROM librat where rating =10 limit 3";
+        $conn=new mysqli(serverName,username,password,dbname);
+        $result = mysqli_query($conn, $sql);
+
+        if(mysqli_num_rows($result) > 0){
+            return $result;
+        }
+ }
 ?>
