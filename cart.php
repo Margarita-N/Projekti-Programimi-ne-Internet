f0ec89751cf63af0a417c931d6514bb96affe2e4
MihrijeK <42340737+MihrijeK@users.noreply.github.com>
2020-05-14 01:29:23 +0200

    Modifikimi i shportes

--- cart.php
+++ cart.php
@@ -1,61 +1,114 @@
-<html>
+<?php
+
+session_start();
+
+require_once ("db/dbconfig.php");
+require_once ("reusable/component.php");
+
+if (isset($_POST['remove'])){
+  if ($_GET['action'] == 'remove'){
+      foreach ($_SESSION['cart'] as $key => $value){
+          if($value["product_id"] == $_GET['id']){
+              unset($_SESSION['cart'][$key]);
+              echo "<script>alert('Produkti u largua nga shporta...!')</script>";
+              echo "<script>window.location = 'cart.php'</script>";
+          }
+      }
+  }
+}
+
+
+?>
+
+<!doctype html>
+<html lang="en">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>winter</title>
-    <link rel="icon" href="img/favicon.png">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- animate CSS -->
-    <link rel="stylesheet" href="css/animate.css">
-    <!-- owl carousel CSS -->
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <!-- font awesome CSS -->
-    <link rel="stylesheet" href="css/all.css">
-    <link rel="stylesheet" href="css/nice-select.css">
-    <!-- flaticon CSS -->
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/themify-icons.css">
-    <!-- font awesome CSS -->
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <!-- swiper CSS -->
-    <link rel="stylesheet" href="css/slick.css">
-    <!-- swiper CSS -->
-    <link rel="stylesheet" href="css/price_rangs.css">
-    <!-- style CSS -->
-    <link rel="stylesheet" href="css/style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <style>.red{
-        color:red;
-    }</style>
-    <script>
-        $(document).ready(function(){
-            $("form").submit(function(event){
-                event.preventDefault();
-                var email=$("#email").val();
-                var password=$("#password").val();
-                var remember=($("#f-option").prop("checked")? 1:0);
-                
-                if(email=="" || password==""){
-                    $.ajax({
-                        url: "validate.php",
-                        data:{email:email,password:password,remember:remember},
-                        type:'POST',
-                        success: function(result){
-                                $(".error-field").html(result);
-                        }});
-                }
-                else{
-                    $.ajax({
-                        url: "validate.php",
-                        data:{email:email,password:password,remember:remember},
-                        type:'POST',
-                        success: function(result){
-                                $(".error-field").html(result);
-                        }});
-                }
-            })
-        })
-    </script>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    
+    <title>Shporta</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
+
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
 </head>
+<body class="bg-light">
+
+<?php
+    require_once ('header.php');
+?>
+
+<div class="container-fluid">
+    <div class="row px-5">
+        <div class="col-md-7">
+            <div class="shopping-cart">
+               
+                <hr>
+
+                <?php
+                $total = 0;
+                    if (isset($_SESSION['cart'])){
+                        $product_id = array_column($_SESSION['cart'], 'product_id');
+
+                        $result = getData();
+                        while ($row = mysqli_fetch_assoc($result)){
+                            foreach ($product_id as $id){
+                                if ($row['bookID'] == $id){
+                                    cartElement($row['fotoPath'], $row['bookName'],$row['price'], $row['bookID']);
+                                    $total = $total + (int)$row['price'];
+                                }
+                            }
+                        }
+                    }else{
+                        echo "<h5>Shporta eshte e zbrazet. </h5>";
+                    }
+
+                ?>
+
+            </div>
+        </div>
+        <div class="col-md-4 offset-md-1 border rounded mt-5 bg-white h-25">
+
+            <div class="pt-4">
+                <h6>Detajet e blerjes</h6>
+                <hr>
+                <div class="row price-details">
+                    <div class="col-md-6">
+                        <?php
+                            if (isset($_SESSION['cart'])){
+                                $count  = count($_SESSION['cart']);
+                                echo "<h6>Cmimi ($count libra)</h6>";
+                            }else{
+                                echo "<h6>Cmimi (0 libra) </h6>";
+                            }
+                        ?>
+                        <hr>
+                        <h6>Shuma per t'u paguar</h6>
+                    </div>
+                    <div class="col-md-6">
+                        <h6>€<?php echo $total; ?></h6>
+                        
+                        <hr>
+                        <h6>€<?php
+                            echo $total;
+                            ?></h6>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
