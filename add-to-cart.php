003e29be873df1ef88e4abebd2d9a5827083dc15
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-05-17 15:39:20 +0200

    Rregullimi per te vendosur me shume se nje liber

--- add-to-cart.php
+++ add-to-cart.php
@@ -6,6 +6,7 @@ require("reusable/product-class.php");
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     if(isset($_SESSION['user'])){
         $id=$_POST['id'];
+        $quantity=$_POST['quantity'];
         $conn=new mysqli(serverName,username,password,dbname);
         $query='SELECT * FROM librat WHERE bookID='.$id;
         $resultArray=mysqli_query($conn,$query);
@@ -16,3 +17,27 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             $price=$result['price'];
             $path=$result['fotoPath'];
         }
+        if(!(($stock-$quantity)<0)){
+            $product=new product($id,$bookName,$price,$path,$quantity);
+    
+            if(!isset($_SESSION['cart'])){
+                $_SESSION['cart']=array();
+                array_push($_SESSION['cart'],$product);
+            }
+            else{
+                array_push($_SESSION['cart'],$product);
+            }
+            echo '<script>alert("Produkti u shtua ne shporte!");</script>';
+        }
+        else{
+            echo '<script>alert("Nuk ka libra ne stock!");</script>';
+        }
+        
+    }
+    else{
+        echo '<script>alert("Nuk mund te shtohet produkti ne shporte pa qene i kyqur!");</script>';
+    }
+    
+}
+
+?>
