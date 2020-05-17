f994fa2487fdae1c9081b3232f76383f70e9829a
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-05-17 21:14:10 +0200

    Shtimi i shop te menyja dhe rregullimi i linqeve

--- AdminHeader.php
+++ AdminHeader.php
@@ -2,32 +2,33 @@
         <div class="nav-menu">
             <div class="home-menu">
             <ul>
-                <li><a href="AdminMain.html"><i class="fas fa-home" style="color:white;font-size: 30px;"></i></a></li>
-                <li><a href="#">Log out</a></li>
+                <li><a href="AdminMain.php"><i class="fas fa-home" style="color:white;font-size: 30px;"></i></a></li>
+                <li><a href="logout.php">Log out</a></li>
+                <li><a href="index.php">Shop</a></li>
             </ul>
             </div>
             <ul class="navbar-nav">
                 <li class="nav-item dropdown"><a href="#">Users</a>
                     <div class="dropdown-menu">
                         <ul>
-                            <li><a href="#" class="dropdown-item">Manage Users</a></li>
-                            <li><a href="#" class="dropdown-item">Add user</a></li>
+                            <li><a href="users_AdminPage.php" class="dropdown-item">Manage Users</a></li>
+                            
                         </ul>
                     </div>
                 </li>
                 <li class="nav-item dropdown"><a href="#">Books</a>
                     <div class="dropdown-menu">
                         <ul>
-                            <li><a href="#" class="dropdown-item">Manage Books</a></li>
-                            <li><a href="#" class="dropdown-item">Add book</a></li>
+                            <li><a href="manage-books.php" class="dropdown-item">Manage Books</a></li>
+                            <li><a href="add-book.php" class="dropdown-item">Add book</a></li>
                         </ul>
                     </div>
                 </li>
                 <li class="nav-item dropdown"><a href="#">Categories</a>
                     <div class="dropdown-menu">
                         <ul>
-                            <li><a href="#" class="dropdown-item">Manage Categories</a></li>
-                            <li><a href="#" class="dropdown-item">Add Categories</a></li>
+                            <li><a href="manage-categories.php" class="dropdown-item">Manage Categories</a></li>
+                            <li><a href="add-category.php" class="dropdown-item">Add Categories</a></li>
                         </ul>
                     </div>
                 </li>
